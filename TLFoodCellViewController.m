57608970a306a6738c7eea33380d76592bc723b1
Said Özcan <saidozcn@gmail.com>
2014-03-16 23:27:12 +0200

    If there is no food for the day, app prints message

--- TLFoodCellViewController.m
+++ TLFoodCellViewController.m
@@ -39,105 +39,109 @@
 
 - (void)setFoodList
 {
+    int yOffset = 0;
     
-//    NSMutableArray *lunchList = [[NSMutableArray alloc] initWithArray:[[_foodDictionary valueForKey:@"main_lunch"] componentsSeparatedByString:@","]];
-//    NSMutableArray *altLunchList = [[NSMutableArray alloc] initWithArray:[[_foodDictionary valueForKey:@"alt_lunch"] componentsSeparatedByString:@","]];
-    
-    NSMutableArray *lunchList = [[NSMutableArray alloc] init];
-    NSLog(@"%d",lunchList.count);
-    
-    NSMutableArray *altLunchList = [[NSMutableArray alloc] init];
-
-    NSMutableArray *dinnerList = [[NSMutableArray alloc] initWithArray:[[_foodDictionary valueForKey:@"main_dinner"] componentsSeparatedByString:@","]];
-    NSMutableArray *altDinnerList = [[NSMutableArray alloc] initWithArray:[[_foodDictionary valueForKey:@"alt_dinner"] componentsSeparatedByString:@","]];
-
     UIFont *labelFont12pt = [UIFont fontWithName:@"Lato-Light" size:12.0f];
     UIFont *headingFont = [UIFont fontWithName:@"Lato-Medium" size:14.0f];
     
-    NSString *lunch = [[NSString alloc] init];
-    int yOffset = 0;
-
+    //setting fonts
     [self.lunchTitle setFont:headingFont];
     [self.dinnerTitle setFont:headingFont];
     [self.dateTitle setFont:headingFont];
+    
+    //settting texts
     _lunchTitle.text = @"Öğle Yemeği";
     _dinnerTitle.text = @"Akşam Yemeği";
     
-    
+    //creating date string
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
     [dateFormat setDateFormat:@"dd.MM.yyyy"];
     [dateFormat setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"tr_TR"]];
     NSDate *date = [dateFormat dateFromString:_foodDate];
     [dateFormat setDateFormat:@"dd MMMM EEEE"];
+    
+    //setting date string
     _dateTitle.text = [dateFormat stringFromDate:date];
     
-    if (nil == lunchList && nil == altLunchList)
-    {
-        UILabel *noFoodLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, _lunchContainer.frame.size.width, 20)];
-        noFoodLabel.text = @"Bugün yemek yok";
-        [noFoodLabel setFont:labelFont12pt];
-        [_lunchContainer addSubview:noFoodLabel];
-    }
+    NSString *lunch = @"";
     
-    for (NSString *anyFood in lunchList)
+    if ([[_foodDictionary valueForKey:@"main_lunch"] length])
     {
-        lunch = [self clearCharactersInString:anyFood];
-        UILabel *lunchLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, _lunchContainer.frame.size.width, 20)];
-        lunchLabel.text = lunch;
-        [lunchLabel setFont:labelFont12pt];
-        [_lunchContainer addSubview:lunchLabel];
-        yOffset += 15;
-        
-    }
+        NSMutableArray *lunchList = [[NSMutableArray alloc] initWithArray:[[_foodDictionary valueForKey:@"main_lunch"] componentsSeparatedByString:@","]];
 
-    for (NSString *anyFood in altLunchList)
-    {
-        lunch = [self clearCharactersInString:anyFood];
-        if ([lunch rangeOfString:@"Vjt"].location == NSNotFound) {
+        for (NSString *anyFood in lunchList)
+        {
+            lunch = [self clearCharactersInString:anyFood];
             UILabel *lunchLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, _lunchContainer.frame.size.width, 20)];
             lunchLabel.text = lunch;
             [lunchLabel setFont:labelFont12pt];
             [_lunchContainer addSubview:lunchLabel];
             yOffset += 15;
+            
         }
-
+        
+        NSMutableArray *altLunchList = [[NSMutableArray alloc] initWithArray:[[_foodDictionary valueForKey:@"alt_lunch"] componentsSeparatedByString:@","]];
+        
+        for (NSString *anyFood in altLunchList)
+        {
+            lunch = [self clearCharactersInString:anyFood];
+            if ([lunch rangeOfString:@"Vjt"].location == NSNotFound) {
+                UILabel *lunchLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, _lunchContainer.frame.size.width, 20)];
+                lunchLabel.text = lunch;
+                [lunchLabel setFont:labelFont12pt];
+                [_lunchContainer addSubview:lunchLabel];
+                yOffset += 15;
+            }
+            
+        }
     }
+    else
+    {
+        UILabel *noFoodLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, _lunchContainer.frame.size.width, 20)];
+        noFoodLabel.text = @"Bugün için öğle yemeği malesef yok.";
+        [noFoodLabel setFont:labelFont12pt];
+        [_lunchContainer addSubview:noFoodLabel];
+    }
+
     
     yOffset = 0;
     
-    if (0 == dinnerList.count && 0 == altDinnerList.count)
+    if ([[_foodDictionary valueForKey:@"main_dinner"] length])
     {
-        UILabel *noFoodLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, _dinnerContainer.frame.size.width, 20)];
-        noFoodLabel.text = @"Bugün yemek yok";
+        NSMutableArray *dinnerList = [[NSMutableArray alloc] initWithArray:[[_foodDictionary valueForKey:@"main_dinner"] componentsSeparatedByString:@","]];
+        
+        for (NSString *anyFood in dinnerList)
+        {
+            lunch = [self clearCharactersInString:anyFood];
+            UILabel *dinnerLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, _dinnerContainer.frame.size.width, 20)];
+            dinnerLabel.text = lunch;
+            [dinnerLabel setFont:labelFont12pt];
+            [_dinnerContainer addSubview:dinnerLabel];
+            yOffset += 15;
+            
+        }
+        
+        NSMutableArray *altDinnerList = [[NSMutableArray alloc] initWithArray:[[_foodDictionary valueForKey:@"alt_dinner"] componentsSeparatedByString:@","]];
+        
+        for (NSString *anyFood in altDinnerList)
+        {
+            lunch = [self clearCharactersInString:anyFood];
+            UILabel *dinnerLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, _dinnerContainer.frame.size.width, 20)];
+            dinnerLabel.text = lunch;
+            [dinnerLabel setFont:labelFont12pt];
+            [_dinnerContainer addSubview:dinnerLabel];
+            yOffset += 15;
+            
+        }
+    }
+    else
+    {
+
+        UILabel *noFoodLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, _dinnerContainer.frame.size.width, 20)];
+        noFoodLabel.text = @"Bugün için akşam yemeği malesef yok.";
         [noFoodLabel setFont:labelFont12pt];
         [_dinnerContainer addSubview:noFoodLabel];
     }
-    
-    for (NSString *anyFood in dinnerList)
-    {
-        lunch = [self clearCharactersInString:anyFood];
-        UILabel *dinnerLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, _dinnerContainer.frame.size.width, 20)];
-        dinnerLabel.text = lunch;
-        [dinnerLabel setFont:labelFont12pt];
-        [_dinnerContainer addSubview:dinnerLabel];
-        yOffset += 15;
-        
-    }
-    
-    for (NSString *anyFood in altDinnerList)
-    {
-        lunch = [self clearCharactersInString:anyFood];
-        UILabel *dinnerLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, yOffset, _dinnerContainer.frame.size.width, 20)];
-        dinnerLabel.text = lunch;
-        [dinnerLabel setFont:labelFont12pt];
-        [_dinnerContainer addSubview:dinnerLabel];
-        yOffset += 15;
-        
-    }
-    
-
-//    _lunchList.text = [objectAtIndex:0];
-//    _dinnerList.text = [[[_foodDictionary valueForKey:@"main_dinner"] componentsSeparatedByString:@","]objectAtIndex:0];
 }
 
 - (NSString *)clearCharactersInString: (NSString *)dirtyString

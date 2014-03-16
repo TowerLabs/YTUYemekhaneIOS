3f15fced891c30eb14829b6c8cfaf184689417bd
Said Özcan <saidozcn@gmail.com>
2014-03-17 00:08:22 +0200

    Renewal again

---
+++ TLFoodCellViewController.h
@@ -0,0 +1,22 @@
+//
+//  TLFoodCellViewController.h
+//  YTUYemekhaneIOS
+//
+//  Created by Davut Ucar on 6.03.2014.
+//  Copyright (c) 2014 TowerLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLFoodCellViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *lunchTitle;
+@property (weak, nonatomic) IBOutlet UIView *lunchContainer;
+
+@property (weak, nonatomic) IBOutlet UILabel *dinnerTitle;
+@property (weak, nonatomic) IBOutlet UIView *dinnerContainer;
+@property (strong, nonatomic) IBOutlet UILabel *dateTitle;
+
+- (id)initWithFoodDictionary:(NSDictionary *)foodDictionary Date: (NSString *)foodDate;
+
+@end
---
+++ YTUYemekhaneIOS/TLAboutCellViewController.m
@@ -0,0 +1,78 @@
+//
+//  TLAboutCellViewController.m
+//  YTUYemekhaneIOS
+//
+//  Created by Meryem on 3/8/14.
+//  Copyright (c) 2014 TowerLabs. All rights reserved.
+//
+
+#import "TLAboutCellViewController.h"
+#import "TLWebViewController.h"
+
+@interface TLAboutCellViewController ()
+
+@property (nonatomic, strong) NSDictionary *infoDictionary;
+
+@end
+
+@implementation TLAboutCellViewController
+
+#pragma mark - LifeCycle
+- (id)initWithDeveloperInfo: (NSDictionary *)infoDictionary
+{
+    if ([[UIScreen mainScreen] bounds].size.height > 480.0f) // retina 4"
+    {
+        self = [super initWithNibName:@"TLAboutCellViewController_4" bundle:nil];
+    }
+    else // retina 3.5"
+    {
+        self = [super initWithNibName:@"TLAboutCellViewController_3" bundle:nil];
+    }
+
+    if (self)
+    {
+        self.infoDictionary = [[NSDictionary alloc] initWithDictionary:infoDictionary];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setInfo];
+}
+
+- (void)setInfo
+{
+#warning TODO: Set the images when they're added to project
+    NSString *name = [[NSString alloc] initWithString:[_infoDictionary valueForKey:@"name"]];
+    NSString *twitter = [[NSString alloc] initWithString:[_infoDictionary valueForKey:@"twitter"]];
+    
+    [_nameLabel setText:name];
+    [_nameLabel setFont:DELEGATE.projectFont];
+    
+    [_siteLabel setTitle:twitter forState:UIControlStateNormal];
+    [_siteLabel.titleLabel setFont:DELEGATE.projectFont];
+    [_icon setImage:[UIImage imageNamed:[_infoDictionary valueForKey:@"image"]]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Action
+- (IBAction)openWebView:(id)sender
+{
+    NSLog(@"Link: %@",[_infoDictionary valueForKey:@"link"]);
+    
+    NSURL *twitterURL = [NSURL URLWithString:[_infoDictionary valueForKey:@"link"]];
+    TLWebViewController *webViewController = [[TLWebViewController alloc] initWithURL:twitterURL];
+    [DELEGATE.navigationController pushViewController:webViewController animated:YES];
+}
+
+
+
+
+@end

a160129f5ffdfbb455d5afb7d8a22635ff35ddaa
Said Özcan <saidozcn@gmail.com>
2014-05-15 01:58:33 +0300

    Added don't have current menu error label.

--- classes/TLFoodViewController.m
+++ classes/TLFoodViewController.m
@@ -103,7 +103,7 @@
     }
     else
     {
-        if(1 == _day)
+        if(1 == _day || 2 == _day)
         {
             NSLog(@"Sifirdan cekiyorum");
             
@@ -180,40 +180,60 @@
 //    NSLog(@"Data: %@",[jsonResponse JSONValue]);
     self.foodArray = [[NSMutableArray alloc]initWithArray:[jsonResponse JSONValue]];
 
-    int keyOfToday = [self getKeyOfToday];
- 
-    NSInteger end;
-    
-    if(_foodArray.count <= keyOfToday+10 )
-    {
-        end = _foodArray.count-1;
+
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitMonth fromDate:[NSDate date]];
+    NSInteger curMonth = [components month];
+   
+    if (curMonth == [[[[[[self.foodArray objectAtIndex:0] allKeys]objectAtIndex:0]componentsSeparatedByString:@"."]objectAtIndex:1]integerValue]){
+        int keyOfToday = [self getKeyOfToday];
+        NSInteger end;
+        
+        if(_foodArray.count <= keyOfToday+10 )
+        {
+            end = _foodArray.count-1;
+        }
+        else
+        {
+            end = keyOfToday + 10;
+        }
+        
+        NSLog(@"%d-%ld",keyOfToday,(long)end);
+        _pageControl.numberOfPages = end - keyOfToday +1;
+        int startX= 0;
+        int startY= 0;
+        int currentX = startX;
+        
+        [_loadingIndicator stopAnimating];
+        
+        for (int i=keyOfToday; i<=end; i++)
+        {
+            
+            TLFoodCellViewController *foodCellViewController = [[TLFoodCellViewController alloc] initWithFoodDictionary:[[[_foodArray objectAtIndex:i] allValues] objectAtIndex:0] Date:[[[_foodArray objectAtIndex:i] allKeys]objectAtIndex:0]];
+            
+            [foodCellViewController.view setFrame:CGRectMake(currentX, startY, foodCellViewController.view.frame.size.width, foodCellViewController.view.frame.size.height)];
+            [_scrollView addSubview:foodCellViewController.view];
+            //        NSLog(@"CurrentX: %d StartY: %d StartX: %d FWidth: %f FHeight: %f SWidth: %f SHeight: %f",currentX, startY, startX, foodCellViewController.view.frame.size.width, foodCellViewController.view.frame.size.height,_scrollView.frame.size.width,_scrollView.frame.size.height);
+            
+            currentX += foodCellViewController.view.frame.size.width;
+        }
+        
+        [_scrollView setContentSize:CGSizeMake(320 * (_pageControl.numberOfPages),_scrollView.contentSize.height)];
     }
     else
     {
-        end = keyOfToday + 10;
+        [_loadingIndicator stopAnimating];
+        _pageControl.numberOfPages = 0;
+        
+        UILabel * dontHaveCurrentMenuLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 100, 280, 200)];
+        dontHaveCurrentMenuLabel.backgroundColor = [UIColor whiteColor];
+        dontHaveCurrentMenuLabel.text = @"Güncel yemek menüsü sks.yildiz.edu.tr adresinde henüz yayınlanmadı.";
+        dontHaveCurrentMenuLabel.font= DELEGATE.projectFont;
+        dontHaveCurrentMenuLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        [self.view addSubview:dontHaveCurrentMenuLabel];
+        dontHaveCurrentMenuLabel.numberOfLines = 3;
+        dontHaveCurrentMenuLabel.textAlignment = NSTextAlignmentCenter;
     }
-    
-    NSLog(@"%d-%ld",keyOfToday,(long)end);
-    _pageControl.numberOfPages = end - keyOfToday +1;
-    int startX= 0;
-    int startY= 0;
-    int currentX = startX;
-    
-    [_loadingIndicator stopAnimating];
-    
-    for (int i=keyOfToday; i<=end; i++)
-    {
-        
-        TLFoodCellViewController *foodCellViewController = [[TLFoodCellViewController alloc] initWithFoodDictionary:[[[_foodArray objectAtIndex:i] allValues] objectAtIndex:0] Date:[[[_foodArray objectAtIndex:i] allKeys]objectAtIndex:0]];
-        
-        [foodCellViewController.view setFrame:CGRectMake(currentX, startY, foodCellViewController.view.frame.size.width, foodCellViewController.view.frame.size.height)];
-        [_scrollView addSubview:foodCellViewController.view];
-//        NSLog(@"CurrentX: %d StartY: %d StartX: %d FWidth: %f FHeight: %f SWidth: %f SHeight: %f",currentX, startY, startX, foodCellViewController.view.frame.size.width, foodCellViewController.view.frame.size.height,_scrollView.frame.size.width,_scrollView.frame.size.height);
-        
-        currentX += foodCellViewController.view.frame.size.width;
-    }
-    
-    [_scrollView setContentSize:CGSizeMake(320 * (_pageControl.numberOfPages),_scrollView.contentSize.height)];
+
 }
 
 #pragma  mark - UIScrollViewDelegate
